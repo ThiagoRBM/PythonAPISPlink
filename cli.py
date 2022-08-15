a52ffc9f12b4ac593bbd011691d4a35c15d17c5e
thiagorbm <thiagorbm@gmail.com>
2022-08-15 06:33:44 -0300

    commit snake case 2

--- cli.py
+++ cli.py
@@ -20,7 +20,7 @@ def buscaSp(
     Argumentos:\n
     especies: OBRIGATÓRIO. A ou as espécies a serem procuradas (pode ser gênero). Pode ser o caminho para um arquivos ".csv" ou ".txt" e nesse caso, as espécies devem estar listadas em uma coluna com nome "Especies".\n
     nivel: OPCIONAL. Por padrão busca por espécie ou gênero. Se "family", busca por espécies da família.\n
-    caminhoTabela: OPCIONAL. Localizacao em que a tabela com as informações das espécies serão salvas.
+    caminho_tabela: OPCIONAL. Localizacao em que a tabela com as informações das espécies serão salvas.
     Se deixado em branco, a tabela será salva no diretório corrente, com o nome 'busca_SPLink_data.csv',
     com a data no formato ano_mes_dia. Fornecer caminho completo + nome do arquivo (e.g. "home/usuario/tabela.csv")
     """
